d0a24602ef71348014d676a86731292aa34051de
Pun Punyasukh <okday13123@gmail.com>
2020-04-26 04:16:31 +0700

    UPdate Lobby

--- Assets/Scripts/Toon/FollowPlayer.cs
+++ Assets/Scripts/Toon/FollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform PlayerTran;
+    public bool stardgo = false;
     public float MoveSpeed = 0.001f;
     void Start()
     {
@@ -13,6 +14,7 @@ public class FollowPlayer : MonoBehaviour
 
     // Update is called once per frame
     void Update () {
+        if(stardgo)
         transform.position = Vector3.MoveTowards(transform.position, PlayerTran.transform.position, MoveSpeed);
     }
 }
